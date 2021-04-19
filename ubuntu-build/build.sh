bc9c15b3821c433f8bdef39988d9b01536f31390
Mark <git@happybeing.com>
2021-04-19 17:58:59 +0100

    Make ready to add timings from Fleming T4

--- ubuntu-build/build.sh
+++ ubuntu-build/build.sh
@@ -5,10 +5,12 @@ set -x
 # Build SAFE CLI on Odroid-U3+, Ubuntu 16.04 LTS / 18.04 LTS
 source $HOME/.cargo/env # If not rebooted since installing Rustc
 
+rm -rf $HOME/.safe
+
 cd $SAFE_SCRIPTS_DIR/sn_api
 git pull
 time cargo update
-time cargo build --release # Took 74 mins
+time cargo build --release # ?? mins (Fleming T?)
 
 # Copy Safe CLI into place
 mkdir -p $SAFE_CLI_DIR
@@ -18,8 +20,6 @@ cp $SAFE_CLI_BUILD $SAFE_CLI_DIR
 mkdir -p $SAFE_AUTH_DIR
 cp $SAFE_AUTHD_BUILD $SAFE_AUTH_DIR
 
-# Select the network (e.g. fleming-testnet)
-$SAFE_CLI networks check
 # If the above complains, add a network config and select it:
 safe networks add fleming-testnet https://sn-node.s3.eu-west-2.amazonaws.com/config/node_connection_info.config
 $SAFE_CLI networks switch fleming-testnet
@@ -31,7 +31,7 @@ source $HOME/.cargo/env # If not rebooted since installing Rustc
 cd $SAFE_SCRIPTS_DIR/sn_node
 git pull
 time cargo update # <10 mins
-time cargo build --release # 56mins
+time cargo build --release # ?? mins (Fleming T?)
 
 # Copy Safe Node into place
 mkdir -p $SAFE_NODE_DIR
