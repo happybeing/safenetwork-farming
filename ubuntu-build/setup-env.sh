bcfc177ede7c36ea491e74c3ad996fd0e472b8c8
Mark <git@happybeing.com>
2021-06-25 15:16:42 +0100

    Fix path to Safe cli

--- ubuntu-build/setup-env.sh
+++ ubuntu-build/setup-env.sh
@@ -9,7 +9,7 @@ export SAFE_NODE_DIR=$HOME/.safe/node
 
 export SAFE_CLI=$SAFE_CLI_DIR/safe
 
-export SAFE_CLI_BUILD=$PWD/sn_api/target/release/safe
+export SAFE_CLI_BUILD=$PWD/sn_cli/target/release/safe
 export SAFE_AUTHD_BUILD=$PWD/sn_api/target/release/sn_authd
 export SAFE_NODE_BUILD=$PWD/safe_network/target/release/sn_node
 
